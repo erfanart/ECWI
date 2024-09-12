63b35c4b135f9d5a7ce3b7d5d018b95d06c3c889
erfanart <erfanart313@gmail.com>
2024-09-12 19:46:07 +0000

    modified:   cert 	modified:   docker-compose.yml 	modified:   entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 # Ensure directories exist
 
-mkdir -p /var/log/apache2
-mkdir -p /etc/apache2
+#mkdir -p /var/log/apache2
+#mkdir -p /etc/apache2
 
 # Copy default configuration files if they don't exist
 if [ ! -d "/var/log/apache2" ]; then
   cp -r /var/log/apache2-default/*  /var/log/apache2/
 fi
 
-if [ ! -d "/etc/apache2/apache2.conf" ]; then
+if [ ! -f "/etc/apache2/apache2.conf" ]; then
   cp -r /etc/apache2-default/* /etc/apache2/
 fi
 
